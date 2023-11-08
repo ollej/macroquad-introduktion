c27195b280cc30cdfff0131611a9c5d99f63217a
Olle Wreede <olle@wreede.se>
2023-11-08 19:14:18 +0100

    Autocommit after building site

--- macroquad-introduction-book/generate-site.sh
+++ macroquad-introduction-book/generate-site.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env sh
 
+SITE_PATH="../../macroquad-introduction-book-site/"
+
 mdbook build
-cp -pR book/* ../../macroquad-introduction-book-site/
+cp -pR book/* "$SITE_PATH"
+cd "$SITE_PATH"
+git commit -am "Update site"
+git push
+cd -
