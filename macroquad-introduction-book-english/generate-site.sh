7b7afed58149945ec88d0446cd579d66d4a7e8af
Olle Wreede <olle@wreede.se>
2024-06-10 19:27:13 +0200

    Copy generated pdf when building site

--- macroquad-introduction-book-english/generate-site.sh
+++ macroquad-introduction-book-english/generate-site.sh
@@ -4,6 +4,7 @@ SITE_PATH="../../macroquad-introduction-book-english-site/"
 
 mdbook build
 cp -pR book/html/* "$SITE_PATH"
+cp book/pdf/output.pdf "$SITE_PATH/pdf/"
 cd "$SITE_PATH"
 git add -A
 git commit -m "Update site"
